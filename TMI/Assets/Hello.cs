633f8573a156a4ea3b9dd557a71376fbf6604c4a
icloud21 <2015152@naver.com>
2021-07-28 16:02:09 +0900

    플레이어 그림 + 헬로

---
+++ TMI/Assets/Hello.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+   
+    void Start()
+    {
+        Debug.Log("Hello");
+    }
+
+}
