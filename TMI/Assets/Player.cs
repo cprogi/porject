cee1793eab6389f65ab1501c8feb2d442ac6056a
icloud21 <2015152@naver.com>
2021-08-12 20:44:40 +0900

    퀘스트 시스템

--- TMI/Assets/Player.cs
+++ TMI/Assets/Player.cs
@@ -7,10 +7,13 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed;
     public float playerHp;
+    public DialogueManager dialogue;
     Rigidbody2D rigid;
     Vector3 touchPos;
     Vector2 dir;
+    Vector2 dirVec;
     SpriteRenderer sprite;
+    GameObject scanObject;
 
     void Start()
     {
@@ -22,14 +25,20 @@ public class Player : MonoBehaviour
     private void Update()
     {
         HealthDown();
+     
         if (Input.GetMouseButton(0))
-            touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        transform.position = Vector2.MoveTowards(transform.position, touchPos,Time.deltaTime* moveSpeed);
+        transform.position = Vector2.MoveTowards(transform.position, touchPos,dialogue.isAction ? 0 : Time.deltaTime* moveSpeed);
         dir = touchPos - transform.position;
-
         sprite.flipX = dir.x > 0;
+        dirVec = dir.normalized;
 
+        //Scan Object
+        if(Input.GetMouseButton(0)&&scanObject != null)
+        {
+            dialogue.Action(scanObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -58,4 +67,15 @@ public class Player : MonoBehaviour
         playerHp -= Time.deltaTime;
     }
     
+    void FixedUpdate()
+    {
+        Debug.DrawRay(rigid.position, dirVec*3f, new Color(0,1,0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 3f, LayerMask.GetMask("Object"));
+        if (rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+            scanObject = null;
+    }
 }
